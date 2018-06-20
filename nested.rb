46a4b29e7ca316dba64959f06c6cb61922a037e4
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-20 14:53:04 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -134,5 +134,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-  return programmer_hash[:dennis_ritchie}
+  return programmer_hash[:dennis_ritchie]
 end
