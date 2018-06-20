58d27f7d87c2f14740395b6dec17dd31fca02b1e
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-20 14:59:39 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -136,3 +136,22 @@ def adding_to_dennis
   programmer_hash[:dennis_ritchie][:languages].push("Assembly")
   return programmer_hash[:dennis_ritchie]
 end
+
+programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+
+puts programmer_hash
