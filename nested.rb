5dfe01f0a5568c78ac042834f03470169ecdc6da
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-20 14:51:19 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -87,9 +87,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  #programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
   return programmer_hash[:yukihiro_matsumoto]
 end
+
+programmer_hash = {}
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+puts programmer_hash
+
 
 def changing_alan
 	programmer_hash = 
